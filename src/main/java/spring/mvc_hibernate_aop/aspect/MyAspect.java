afc03d72b52946e068dd5212a5507c2b82882e8a
RasimAlimgulov <al_rasim98@mail.ru>
2024-05-07 14:14:58 +0500

    Added AOP and LOG4J

--- src/main/java/spring/mvc_hibernate_aop/aspect/MyAspect.java
+++ src/main/java/spring/mvc_hibernate_aop/aspect/MyAspect.java
@@ -1,5 +1,7 @@
 package spring.mvc_hibernate_aop.aspect;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -9,18 +11,22 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class MyAspect {
+    private static final Logger logger = LogManager.getLogger(MyAspect.class);
     @Around("execution(* spring.mvc_hibernate_aop.dao.*.*(..))")
     public Object aroundAllMethodsInDaoAdvise(ProceedingJoinPoint joinPoint){
         MethodSignature methodSignature=(MethodSignature) joinPoint.getSignature();
        String nameMethod=methodSignature.getMethod().getName();
-        System.out.println("Start of "+nameMethod);
+      //  System.out.println("Start of "+nameMethod);
         Object result;
+        logger.info("Started method : "+nameMethod+"\n\n\n");
         try {
             result=joinPoint.proceed();
         } catch (Throwable e) {
+            logger.error("Произошла ошибка в DAO методах: ",e);
             throw new RuntimeException(e);
         }
-        System.out.println("End of "+nameMethod);
+        logger.info("Method finished : "+nameMethod+"\n\n\n");
+      //  System.out.println("End of "+nameMethod);
         return result;
     }
 }
